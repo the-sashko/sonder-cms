c6bb2e3865e47bb33ade305ed65f5017d546ced1
The Sashko <inbox@sashko.me>
2021-11-14 19:36:41 +0200

    improve version plugin

---
+++ plugins/version/VersionPlugin.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Sonder\Plugins;
+
+final class VersionPlugin
+{
+    const CONFIG_FILE_PATHS = [
+        __DIR__ . '/../../../config/cms.json',
+        __DIR__ . '/../../config/cms.json'
+    ];
+
+    /**
+     * @var array
+     */
+    private array $_configValues = [];
+
+    final public function __construct()
+    {
+        foreach (VersionPlugin::CONFIG_FILE_PATHS as $configFilePath) {
+            if (file_exists($configFilePath) && is_file($configFilePath)) {
+                continue;
+            }
+
+            $configValues = file_get_contents($configFilePath);
+            $configValues = json_decode($configValues, true);
+
+            $this->_configValues = (array)$configValues;
+
+            break;
+        }
+    }
+
+    /**
+     * @return string|null
+     */
+    final public function getVersion(): ?string
+    {
+        if (
+            array_key_exists('version', $this->_configValues) &&
+            !empty($this->_configValues['version']) &&
+            is_scalar($this->_configValues['version'])
+        ) {
+            return (string)$this->_configValues['version'];
+        }
+
+        return null;
+    }
+
+    /**
+     * @return bool
+     */
+    final public function getLock(): bool
+    {
+        if (
+            array_key_exists('lock', $this->_configValues) &&
+            !empty($this->_configValues['lock']) &&
+            is_scalar($this->_configValues['lock'])
+        ) {
+            return $this->_configValues['lock'] == 'true';
+        }
+
+        return false;
+    }
+}
