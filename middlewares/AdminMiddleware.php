20c6e45e1b88a42e9c45403977ace45039efc70f
The Sashko <inbox@sashko.me>
2021-11-14 19:41:32 +0200

    improve admin middleware

--- middlewares/AdminMiddleware.php
+++ middlewares/AdminMiddleware.php
@@ -1,2 +1,53 @@
 <?php
-//TODO
+
+namespace Sonder\Middlewares;
+
+use Exception;
+use Sonder\Controllers\AdminController;
+use Sonder\Core\CoreMiddleware;
+use Sonder\Core\Interfaces\IMiddleware;
+use Sonder\Core\ResponseObject;
+
+final class AdminMiddleware extends CoreMiddleware implements IMiddleware
+{
+    final public function run(): void
+    {
+        $isSignedIn = false;
+
+        $user = $this->request->getUser();
+
+        if (
+            !empty($user) &&
+            !empty($user->getId()) &&
+            !empty($user->getRole()) &&
+            $user->getRole()->can(
+                AdminController::USER_ACTION_ADMIN
+            )
+        ) {
+            $isSignedIn = true;
+        }
+
+        if (
+            !$isSignedIn &&
+            $this->request->getUrl() != AdminController::SIGN_IN_URL
+        ) {
+            $this->_redirect(AdminController::SIGN_IN_URL);
+        }
+
+        if (
+            $isSignedIn &&
+            $this->request->getUrl() == AdminController::SIGN_IN_URL
+        ) {
+            $this->_redirect(AdminController::ADMIN_INDEX_URL);
+        }
+    }
+
+    /**
+     * @param string $url
+     */
+    private function _redirect(string $url): void
+    {
+        $this->response = new ResponseObject();
+        $this->response->redirect->setUrl($url);
+    }
+}
