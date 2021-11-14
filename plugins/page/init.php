98a973478910108b1ddd3e12bbb8d72bacfb1d51
The Sashko <inbox@sashko.me>
2021-11-14 19:39:54 +0200

    improve page plugin

---
+++ plugins/page/init.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/../../framework/core/values.object.class.php';
+require_once __DIR__ . '/PageException.php';
+require_once __DIR__ . '/PageValuesObject.php';
+require_once __DIR__ . '/PagePlugin.php';
