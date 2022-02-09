5fef3b78fa24c3c18bc7bd336498520e8f3e35a3
The Sashko <inbox@sashko.me>
2022-02-09 04:07:26 +0200

    improve cron admin controller

--- controllers/admin/AdminCronController.php
+++ controllers/admin/AdminCronController.php
@@ -4,6 +4,9 @@ namespace Sonder\Controllers;
 
 use Exception;
 use Sonder\Core\ResponseObject;
+use Sonder\Models\Cron;
+use Sonder\Models\Cron\CronForm;
+use Sonder\Models\Cron\CronValuesObject;
 use Sonder\Plugins\Database\Exceptions\DatabaseCacheException;
 use Sonder\Plugins\Database\Exceptions\DatabasePluginException;
 
@@ -22,12 +25,42 @@ final class AdminCronController extends AdminBaseController
      */
     final public function displayCron(): ResponseObject
     {
-        //TODO
+        /* @var $cronModel Cron */
+        $cronModel = $this->getModel('cron');
+
+        $cronJobs = $cronModel->getCronJobsByPage($this->page);
+        $pageCount = $cronModel->getCronJobsPageCount();
+
+        if (empty($cronJobs) && $this->page > 1) {
+            return $this->redirect('/admin/settings/cron/');
+        }
+
+        if (($this->page > $pageCount) && $this->page > 1) {
+            return $this->redirect('/admin/settings/cron/');
+        }
+
+        $pagination = $this->getPlugin('paginator')->getPagination(
+            $pageCount,
+            $this->page,
+            '/admin/settings/cron/'
+        );
+
+        $this->assign([
+            'cron_jobs' => $cronJobs,
+            'pagination' => $pagination,
+            'page_path' => [
+                '/admin/' => 'Admin',
+                '/admin/settings/' => 'Settings',
+                '#' => 'Cron'
+            ]
+        ]);
+
+        return $this->render('settings/cron/list');
     }
 
     /**
      * @area admin
-     * @route /admin/settings/cron/view/([0-9]+)/
+     * @route /admin/settings/cron/job/view/([0-9]+)/
      * @url_params id=$1
      * @no_cache true
      *
@@ -38,12 +71,38 @@ final class AdminCronController extends AdminBaseController
      */
     final public function displayCronJob(): ResponseObject
     {
-        //TODO
+        /* @var $cronModel Cron */
+        $cronModel = $this->getModel('cron');
+
+        if (empty($this->id)) {
+            return $this->redirect('/admin/settings/cron/');
+        }
+
+        /* @var $cronVO CronValuesObject */
+        $cronVO = $cronModel->getVOById($this->id);
+
+        if (empty($cronVO)) {
+            return $this->redirect('/admin/settings/cron/');
+        }
+
+        $pagePath = [
+            '/admin/' => 'Admin',
+            '/admin/settings/' => 'Users',
+            '/admin/settings/cron/' => 'Cron',
+            '#' => sprintf('ID: %d', $cronVO->getId())
+        ];
+
+        $this->assign([
+            'cron_job' => $cronVO,
+            'page_path' => $pagePath
+        ]);
+
+        return $this->render('settings/cron/view');
     }
 
     /**
      * @area admin
-     * @route /admin/settings/cron((/([0-9]+)/)|/)
+     * @route /admin/settings/cron/job((/([0-9]+)/)|/)
      * @url_params id=$3
      * @no_cache true
      *
@@ -54,12 +113,90 @@ final class AdminCronController extends AdminBaseController
      */
     final public function displayCronJobForm(): ResponseObject
     {
-        //TODO
+        $id = $this->id;
+
+        $errors = [];
+
+        $action = null;
+        $interval = null;
+        $isActive = true;
+
+        $cronVO = null;
+        $cronForm = null;
+
+        $pageTitle = 'new';
+
+        /* @var $cronModel Cron */
+        $cronModel = $this->getModel('cron');
+
+        if (!empty($id)) {
+            /* @var $cronVO CronValuesObject */
+            $cronVO = $cronModel->getVOById($id);
+            $pageTitle = 'Edit';
+        }
+
+        if (!empty($id) && empty($cronVO)) {
+            return $this->redirect('/admin/settings/cron/job/');
+        }
+
+        if ($this->request->getHttpMethod() == 'post') {
+            /* @var $cronForm CronForm */
+            $cronForm = $cronModel->getForm($this->request->getPostValues());
+
+            $cronModel->save($cronForm);
+        }
+
+        if (!empty($cronForm) && $cronForm->getStatus()) {
+            $id = $cronForm->getId();
+
+            $urlPattern = '/admin/settings/cron/job/view/%d/';
+            $url = '/admin/settings/cron/';
+
+            $url = empty($id) ? $url : sprintf($urlPattern, $id);
+
+            return $this->redirect($url);
+        }
+
+        if (!empty($cronForm)) {
+            $errors = $cronForm->getErrors();
+        }
+
+        if (!empty($cronVO)) {
+            $action = $cronVO->getAction();
+            $interval = $cronVO->getInterval();
+            $isActive = $cronVO->getIsActive();
+        }
+
+        if (!empty($cronForm)) {
+            $action = $cronForm->getAction();
+            $interval = $cronForm->getInterval();
+            $isActive = $cronForm->getIsActive();
+        }
+
+        //TODO: get all available cron actions
+
+        $pagePath = [
+            '/admin/' => 'Admin',
+            '/admin/settings/' => 'Settings',
+            '/admin/settings/cron/' => 'Cron',
+            '#' => $pageTitle
+        ];
+
+        $this->assign([
+            'id' => $id,
+            'action' => $action,
+            'interval' => $interval,
+            'is_active' => $isActive,
+            'errors' => $errors,
+            'page_path' => $pagePath
+        ]);
+
+        return $this->render('settings/cron/form');
     }
 
     /**
      * @area admin
-     * @route /admin/settings/cron/remove/([0-9]+)/
+     * @route /admin/settings/cron/job/remove/([0-9]+)/
      * @url_params id=$1
      * @no_cache true
      *
@@ -69,12 +206,24 @@ final class AdminCronController extends AdminBaseController
      */
     final public function displayRemoveCronJob(): ResponseObject
     {
-        //TODO
+        /* @var $cronModel Cron */
+        $cronModel = $this->getModel('cron');
+
+        if (!$cronModel->removeCronJobById($this->id)) {
+            $loggerPlugin = $this->getPlugin('logger');
+
+            $errorMessage = 'Can Not Remove Cron Job With "%d"';
+            $errorMessage = sprintf($errorMessage, $this->id);
+
+            $loggerPlugin->logError($errorMessage);
+        }
+
+        return $this->redirect('/admin/settings/cron/');
     }
 
     /**
      * @area admin
-     * @route /admin/settings/cron/restore/([0-9]+)/
+     * @route /admin/settings/cron/job/restore/([0-9]+)/
      * @url_params id=$1
      * @no_cache true
      *
@@ -83,6 +232,33 @@ final class AdminCronController extends AdminBaseController
      * @throws Exception
      */
     final public function displayRestoreCronJob(): ResponseObject
+    {
+        /* @var $cronModel Cron */
+        $cronModel = $this->getModel('cron');
+
+        if (!$cronModel->restoreCronJobById($this->id)) {
+            $loggerPlugin = $this->getPlugin('logger');
+
+            $errorMessage = 'Can Not Restore Cron Job With "%d"';
+            $errorMessage = sprintf($errorMessage, $this->id);
+
+            $loggerPlugin->logError($errorMessage);
+        }
+
+        return $this->redirect('/admin/settings/cron/');
+    }
+
+    /**
+     * @area admin
+     * @route /admin/settings/cron/job/run/([0-9]+)/
+     * @url_params id=$1
+     * @no_cache true
+     *
+     * @return ResponseObject
+     * @throws DatabasePluginException
+     * @throws Exception
+     */
+    final public function displayRunCronJob(): ResponseObject
     {
         //TODO
     }
