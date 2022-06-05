af0ad3dea73c1401cc175e6a37b5cc06f76ab767
The Sashko <inbox@sashko.me>
2022-06-05 17:30:24 +0300

    improve controllers

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -5,12 +5,14 @@ namespace Sonder\Controllers;
 use Exception;
 use Sonder\CMS\Essentials\BaseController;
 use Sonder\Core\ResponseObject;
+use Sonder\Models\Article;
+use Sonder\Models\Article\ArticleValuesObject;
 
 final class ArticleController extends BaseController
 {
     /**
      * @area blog
-     * @route /p/([a-z]+)/
+     * @route /p/([0-9a-z-]+)/
      * @url_params slug=$1
      *
      * @return ResponseObject
@@ -19,10 +21,29 @@ final class ArticleController extends BaseController
      */
     final public function displaySingle(): ResponseObject
     {
-        return $this->render('index');
+        $slug = $this->request->getUrlValue('slug');
 
-        //TODO
+        if (empty($slug)) {
+            return $this->redirect($this->getNotFoundUrl(), true);
+        }
+
+        /* @var $articleModel Article|null */
+        $articleModel = $this->getModel('article');
+
+        $article = $articleModel->getVOBySlug(
+            $slug,
+            true,
+            true
+        );
+
+        if (empty($article)) {
+            return $this->redirect($this->getNotFoundUrl(), true);
+        }
+
+        $this->assign([
+            'article' => $article
+        ]);
+
+        return $this->render('article');
     }
-
-    //TODO
 }
--- controllers/MainController.php
+++ controllers/MainController.php
@@ -5,23 +5,80 @@ namespace Sonder\Controllers;
 use Exception;
 use Sonder\CMS\Essentials\BaseController;
 use Sonder\Core\ResponseObject;
+use Sonder\Models\Article;
+use Sonder\Plugins\Database\Exceptions\DatabaseCacheException;
+use Sonder\Plugins\Database\Exceptions\DatabasePluginException;
 
 final class MainController extends BaseController
 {
+    const MAIN_PAGE_URL = '/';
+
     /**
      * @area blog
      * @route /
+     * @url_params page=$3
+     *
+     * @return ResponseObject
+     * @throws DatabaseCacheException
+     * @throws DatabasePluginException
+     * @throws Exception
+     */
+    final public function displayIndex(): ResponseObject
+    {
+        /* @var Article $articleModel */
+        $articleModel = $this->getModel('article');
+
+        $articles = $articleModel->getArticlesByPage(
+            $this->page,
+            true,
+            true
+        );
+
+        if (empty($articles) && $this->page > 1) {
+            return $this->redirect(MainController::MAIN_PAGE_URL);
+        }
+
+        if (empty($articles)) {
+            return $this->redirect($this->getNotFoundUrl());
+        }
+
+        $this->assign([
+            'current_page' => $this->page,
+            'articles' => $articles
+        ]);
+
+        return $this->render('home_page');
+    }
+
+    /**
+     * @area blog
+     * @route /demo/
      *
      * @return ResponseObject
      *
      * @throws Exception
      */
-    final public function displayIndex(): ResponseObject
+    final public function displayDemo(): ResponseObject
     {
         return $this->render('demo');
-
-        //TODO
     }
 
-    //TODO
+    /**
+     * @area blog
+     * @route /not-found/
+     *
+     * @return ResponseObject
+     *
+     * @throws Exception
+     */
+    final public function displayNotFound(): ResponseObject
+    {
+        if ($this->request->getUrl() != $this->getNotFoundUrl()) {
+            return $this->redirect($this->getNotFoundUrl());
+        }
+
+        $this->response->setHttpCode(ResponseObject::NOT_FOUND_HTTP_CODE);
+
+        return $this->render('not_found');
+    }
 }
