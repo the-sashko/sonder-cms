b3bf55fdc617550abd671bd90eacf9e6eeb50324
The Sashko <inbox@sashko.me>
2022-07-12 14:00:47 +0300

    improve blog.init.php

--- blog.init.php
+++ blog.init.php
@@ -14,6 +14,8 @@ ini_set('display_startup_errors', 1);
 
 
 //TODO: refactoring
+require_once __DIR__ . '/init.php';
+/*
 if (
     file_exists(__DIR__ . '/../../vendor/autoload.php') &&
     is_file(__DIR__ . '/../../vendor/autoload.php')
@@ -22,3 +24,4 @@ if (
 } else {
     require_once __DIR__ . '/init.php';
 }
+*/
